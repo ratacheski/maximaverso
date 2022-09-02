890d45cc88435a497236b6c582fdd9d1504c75c3
Rafael Ratacheski <rafael.raulino@maximatech.com.br>
2022-09-02 14:49:18 -0300

    Remove Jest Test temporally

--- test/MaximaVerse.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import MaximaVerse from '~/components/MaximaVerse'
-
-describe('MaximaVerse', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(MaximaVerse)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
